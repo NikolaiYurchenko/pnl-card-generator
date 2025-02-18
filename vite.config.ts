e252cf56cbf87121c89178f2e24de8dc3658b332
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2025-02-18 16:33:30 +0100

    resolve path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pnl-card-generator'
+  base: '/pnl-card-generator',
+  build: {
+    minify: "esbuild",
+    outDir: resolve(__dirname, "dist/"),
+    cssCodeSplit: false,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@assets": resolve(__dirname, "src/assets"),
+    }
+  }
 })
